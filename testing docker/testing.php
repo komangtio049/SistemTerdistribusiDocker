7f4c1a90f4f953c6c763f725919a7ae62fcec9fc
komangtio049 <127291407+komangtio049@users.noreply.github.com>
2024-07-09 02:17:25 +0800

    Add files via upload

---
+++ testing docker/testing.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <style>
+        body {
+            background-color: white;
+            color: white;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            margin: 0;
+        }
+        .container {
+            text-align: center;
+            background: black;
+            padding: 20px;
+            border-radius: 8px;
+            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.3);
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <?php
+        echo "Pande Komang Tio Gemisatya<br>";
+        echo "2215101049<br><br>";
+        echo "Praktikum Docker";
+        ?>
+    </div>
+</body>
+</html>
